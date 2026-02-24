2d714d39e4825fd4e895bd0788336cd5b3c1872b
samarasekhark <samar.kodavatikanti@gmail.com>
2026-02-25 02:13:20 +0530

    fix(frontend): restore firebase config variables required for app bootstrap in prod environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: true,
-
+    firebase: {
+        apiKey: "AIzaSyAXc4ZGRdKNaWsNAHDZeEJNhrEHJEgz6F0",
+        authDomain: "tracker-e40bd.firebaseapp.com",
+        projectId: "tracker-e40bd",
+        storageBucket: "tracker-e40bd.firebasestorage.app",
+        messagingSenderId: "460912154731",
+        appId: "1:460912154731:web:dda03aaf06b1a06fe9792e",
+        measurementId: "G-8ZS0MJRMK8"
+    }
 };
